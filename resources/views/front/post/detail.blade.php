83d4107d3da9e72c35ccbcc67b63d533f8c378ad
maungmatubbar <maungmatubbar@gmail.com>
2022-10-11 19:23:01 +0600

    Update comment section

--- resources/views/front/post/detail.blade.php
+++ resources/views/front/post/detail.blade.php
@@ -63,87 +63,92 @@
             </div>
         </div>
         @foreach($comments as $comment)
-            <div class="card m-0 card-body mb-2 rounded-0" id="commentBox-{{ $comment->id }}">
-                <div class="row">
-                    <div class="card-text col-md-9">
-                        <h5 class="p-0">{{ $comment->name }}</h5>
-                        <p class="p-0">{{ date('M d,Y',strtotime( $comment->created_at))  }}</p>
-                        <p class="p-0">{{ $comment->comment }} by {{ $comment->name }}</p>
-                    </div>
-                    <div class="col-md-3 mt-4">
-                        <div class="mt-4">
-                            <button class="btn btn-info reply_btn" replyBoxId="{{ $comment->id }} {{ Auth::guard('web')->user()?'':'disabled' }}">Reply</button>
-                            <button class="btn btn-success comment_hide" {{ Auth::guard('admin')->user()?'':' disabled' }} comment_id="{{ $comment->id }}">Hide</button>
+            <div>
+                <div class="card m-0 card-body mb-2 rounded-0" id="commentBox-{{ $comment->id }}">
+                    <div class="row">
+                        <div class="card-text col-md-9">
+                            <h5 class="p-0">{{ $comment->name }}</h5>
+                            <p class="p-0">{{ date('M d,Y',strtotime( $comment->created_at))  }}</p>
+                            <p class="p-0">{{ $comment->comment }} by {{ $comment->name }}</p>
+                        </div>
+                        <div class="col-md-3 mt-4">
+                            <div class="mt-4">
+                                <button class="btn btn-info reply_btn" replyBoxId="{{ $comment->id }} {{ Auth::guard('web')->user()?'':'disabled' }}">Reply</button>
+                                <button class="btn btn-success comment_hide" {{ Auth::guard('admin')->user()?'':' disabled' }} comment_id="{{ $comment->id }}">Hide</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card mb-2 border-0 replyBox"  id="{{ $comment->id }}">
-                    <div class="card-body">
-                        <h3 class="card-title">
-                            Reply
-                        </h3>
-                        <form action="{{ route('reply.store') }}" method="post">@csrf
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <input type="hidden" name="comment_id" value="{{ $comment->id }}">
-                                    <input type="hidden" name="parent_id" value="0">
-                                    <textarea name="message" id="" cols="30" rows="5" placeholder="Reply message" class="form-control shadow"></textarea>
+                    <div class="card mb-2 border-0 replyBox"  id="{{ $comment->id }}">
+                        <div class="card-body">
+                            <h3 class="card-title">
+                                Reply
+                            </h3>
+                            <form action="{{ route('reply.store') }}" method="post">@csrf
+                                <div class="row mb-3">
+                                    <div class="col-md-12">
+                                        <input type="hidden" name="comment_id" value="{{ $comment->id }}">
+                                        <input type="hidden" name="parent_id" value="0">
+                                        <textarea name="message" id="" cols="30" rows="5" placeholder="Reply message" class="form-control shadow"></textarea>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <input type="submit" value="Reply" class="btn btn-success" {{ Auth::guard('web')->user()?'':'disabled' }}>
+                                <div class="row mb-3">
+                                    <div class="col-md-12">
+                                        <input type="submit" value="Reply" class="btn btn-success" {{ Auth::guard('web')->user()?'':'disabled' }}>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
 
+                        </div>
                     </div>
+
                 </div>
                 @foreach($comment->replies as $reply)
-                    <div class="card card-body ml-5 mb-2" id="reply-{{ $reply->id }}">
-                        <div class="row">
-                            <div class="card-text col-md-9">
-                                <h5 class="p-0">{{ $reply->name }}</h5>
-                                <p class="p-0">{{ date('M d,Y',strtotime( $reply->created_at)) }}</p>
-                                <p class="p-0">{{ $reply->message }} by {{ $reply->name }}</p>
-                            </div>
-                            <div class="col-md-3 mt-4">
-                                <div class="btn-group">
-                                    <button class="btn btn-secondary subReply_btn {{ Auth::guard('web')->user()?'':'disabled' }}" subReplyBoxId="{{ $reply->id }}">Reply</button>
-                                    <button class="btn btn-success replyHide" reply_id="{{ $reply->id }}" {{ Auth::guard('admin')->user()?'':'disabled' }}>Hide</button>
+                    <div>
+                        <div class="card card-body ml-5 mb-2" id="reply-{{ $reply->id }}">
+                            <div class="row">
+                                <div class="card-text col-md-9">
+                                    <h5 class="p-0">{{ $reply->name }}</h5>
+                                    <p class="p-0">{{ date('M d,Y',strtotime( $reply->created_at)) }}</p>
+                                    <p class="p-0">{{ $reply->message }} by {{ $reply->name }}</p>
+                                </div>
+                                <div class="col-md-3 mt-4">
+                                    <div class="btn-group">
+                                        <button class="btn btn-secondary subReply_btn {{ Auth::guard('web')->user()?'':'disabled' }}" subReplyBoxId="{{ $reply->id }}">Reply</button>
+                                        <button class="btn btn-success replyHide" reply_id="{{ $reply->id }}" {{ Auth::guard('admin')->user()?'':'disabled' }}>Hide</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card mb-2 border-0 subReply" id="subReply-{{ $reply->id }}">
-                            <div class="card-body">
-                                <h3 class="card-title">
-                                    Reply
-                                </h3>
-                                <form action="{{ route('reply.store') }}" method="post">@csrf
-                                    <div class="row mb-3">
-                                        <div class="col-md-12">
-                                            <input type="hidden" name="comment_id" value="{{ $comment->id }}">
-                                            <input type="hidden" name="parent_id" value="{{ $reply->id }}">
-                                            <textarea name="message" id="" cols="30" rows="5" placeholder="Reply message" class="form-control shadow"></textarea>
+                            <div class="card mb-2 border-0 subReply" id="subReply-{{ $reply->id }}">
+                                <div class="card-body">
+                                    <h3 class="card-title">
+                                        Reply
+                                    </h3>
+                                    <form action="{{ route('reply.store') }}" method="post">@csrf
+                                        <div class="row mb-3">
+                                            <div class="col-md-12">
+                                                <input type="hidden" name="comment_id" value="{{ $comment->id }}">
+                                                <input type="hidden" name="parent_id" value="{{ $reply->id }}">
+                                                <textarea name="message" id="" cols="30" rows="5" placeholder="Reply message" class="form-control shadow"></textarea>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row mb-3">
-                                        <div class="col-md-12">
-                                            <input type="submit" value="Reply" class="btn btn-success" {{ Auth::guard('web')->user()?'':'disabled' }}>
+                                        <div class="row mb-3">
+                                            <div class="col-md-12">
+                                                <input type="submit" value="Reply" class="btn btn-success" {{ Auth::guard('web')->user()?'':'disabled' }}>
+                                            </div>
                                         </div>
-                                    </div>
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                         @foreach($reply->subreplies as $subreply)
-                            <div class="card card-body ml-5 mb-2" id="reply-{{ $subreply->id }}">
+                            <div class="card card-body mb-2" id="reply-{{ $subreply->id }}" style="left: 100px; display: inline-block; padding-right: 0;margin-right: 98px !important;">
                                 <div class="row">
                                     <div class="card-text col-md-9">
                                         <h5 class="p-0">{{ $subreply->name }}</h5>
                                         <p class="p-0">{{ date('M d,Y',strtotime( $subreply->created_at)) }}</p>
                                         <p class="p-0">{{ $subreply->message }} by {{ $subreply->name }}</p>
                                     </div>
-                                    <div class="col-md-3 mt-4">
+                                    <div class="col-md-3 mt-5">
                                         <div class="btn-group">
                                             <button class="btn btn-success replyHide" reply_id="{{ $subreply->id }}" {{ Auth::guard('admin')->user()?'':'disabled' }}>Hide</button>
                                         </div>
@@ -152,6 +157,7 @@
                             </div>
                         @endforeach
                     </div>
+
                 @endforeach
             </div>
         @endforeach
